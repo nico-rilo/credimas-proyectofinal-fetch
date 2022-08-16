e1b15095e9df21e37a40a10a4aadaec5546be828
nico-rilo <nicolasrilo@live.com.ar>
2022-08-15 22:14:28 -0300

    cambios en js/main agrego librerias

--- js/main.js
+++ js/main.js
@@ -2,33 +2,24 @@ let formulario = document.getElementById("formulario");
 let inversion = document.getElementById("inversion");
 let boton = document.getElementById("calcular");
 let contenedor = document.getElementById("contenedor");
-let rangoInversion;
+let rangoInversion
 
 let precio = 0;
-const intereses = [1.1, 1.2, 1.3, 1.4, 1.5];
+const intereses = [1.1, 1.2, 1.3, 1.4, 1.5]
 
-const PlazosFijos = JSON.parse(localStorage.getItem("plazosFijos")) || [];
+let PlazosFijos = JSON.parse(localStorage.getItem("plazosFijos")) || []; 
 
 class plazoFijoUsuario {
     constructor(monto, interes) {
-        this.monto = monto;
-        this.interes = interes;
-        this.deposito = this.monto * this.interes;
+        this.monto = monto,
+        this.interes = interes,
+        this.deposito = this.monto * this.interes
     }
-}
-
-const validarCampos = () => {
-    if (!inversion.value) {
-        alert("Debe llenar el campo con números");
-        return false;
-    } else {
-        return true;
-    };
 }
 
 const crearTarjeta = () => {
 
-    inversionIngresada = parseInt(inversion.value);
+        inversionIngresada = parseInt(inversion.value)
 
     if (inversionIngresada <= 1000) {
         rangoInversion = ("Depositando ese monto tenés el 10% de interés anual, por lo tanto tu depósito se convertirá en " + "$" + inversionIngresada * intereses[0]);
@@ -51,6 +42,8 @@ const crearTarjeta = () => {
         PlazosFijos.push(new plazoFijoUsuario(inversionIngresada, intereses[4]));
     }
 
+    console.log (inversionIngresada?.a || "el interés no existe")
+
     localStorage.setItem("plazosFijos", JSON.stringify(PlazosFijos));
 
     contenedor.innerHTML = `<div class="card col-4 mx-1 p-3">
@@ -63,10 +56,9 @@ const crearTarjeta = () => {
 }
 
 boton.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (validarCampos()) {
-        crearTarjeta()
-    };
+    e.preventDefault();    
+        crearTarjeta();
+   
 });
 
 let contenedorPlazos = document.getElementById("contenedorPlazos");
@@ -77,7 +69,7 @@ const verHistorial = () => {
     contenedorPlazos.innerHTML = "";
     PlazosFijos.forEach((plazo) => {
         contenedorPlazos.innerHTML += `<li> Tenés un plazo de ${plazo.monto}, que se convertiran en ${plazo.deposito}.<li>`;
-    });
+    })
 };
 
 const mostrarTotal = () => {
@@ -92,6 +84,7 @@ const mostrarTotal = () => {
 btnHistorial.addEventListener("click", () => {
     verHistorial();
     mostrarTotal();
+    5
 })
 
 function borrarHistorial() {
@@ -107,25 +100,79 @@ btnBorrarHistorial.addEventListener('click', (event) => {
     borrarHistorial();
     verHistorial();
     mostrarTotal();
-
+    
     contenedor.innerHTML = `<div class="card col-4 mx-1 p-3">
                                 <h3> Gracias por utilizar nuestro simulador </h3>
                                 <p>Esperamos tu inversión!!</p>
                             </div>`;
 
     formulario.reset();
+    Swal.fire({
+        title: 'VAMOOOO METÉLE PESOS ARGENTOS!!!!',
+        text: 'Muerte al dolar yankeeee',
+        imageUrl: 'http://c.files.bbci.co.uk/6AD9/production/_103735372_gettyimages-175494583.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Dolar quemado',
+      })
+    
 });
 
-const titulo = (document.querySelector("h1").textContent = "CREDIMAS, es mejor.");
-const subTitulo = (document.querySelector(".text-white-50").textContent = "Happiness is a warm gun");
-const titulo2 = (document.querySelector("h2").textContent = "Alli y para siempre aprendimos que ciertos fuegos no se encienden frotando dos palitos ni se apagan con solo soplar");
-const descripcion = (document.querySelector(".lead").textContent = "Cuando el carro anda, los melones se acomodan");
+const plazoFijoBanco = {
+    cliente: "Alberto",
+    cuentas: {
+        ctaCorriente: 0203,
+        cajaAhorroEnPesos: 5555,
+        cahaAhorroenDolares: 2258,
+    }
+
+}
+
+console.log (plazoFijoBanco?.cuentas?.cajaAhorroEnPesos || "Esa cuenta no existe")
+
+console.log (plazoFijoBanco?.cuentas?.cajaDeSeguridad || "Esa cuenta no existe") 
+
+const plazoFijoBancoR = {
+    cliente: "Alberto",
+    cuentas: {
+        ctaCorriente: 3245,
+        cajaAhorroEnPesos: 5555,
+        cahaAhorroenDolares: 2258,
+    }
+
+}
+
+const { cliente, cuentas} = plazoFijoBancoR
+
+console.log (cliente) 
+console.log (cuentas) 
+
+const plazoFijoLaucha = {
+    cliente: "Alberto",
+    cuentas: {
+        ctaCorriente: 3245,
+        cajaAhorroEnPesos: 5555,
+        cahaAhorroenDolares: 2258,
+    }
+
+}
+
+const plazoFijoLaucha2 = {
+    ...plazoFijoLaucha
+}
+
+console.log (plazoFijoLaucha2) 
+
+const titulo = (document.querySelector("h1").textContent = "Banco LAUCHISMO");
+const subTitulo = (document.querySelector(".text-white-50").textContent = "Entre la espada y la pared, siempre");
+const titulo2 = (document.querySelector("h2").textContent = "Somos los mas delincuentes del mercado");
+const descripcion = (document.querySelector(".lead").textContent = "Estamos para defondar tus ahorros y hacerte creer que ganarás dinero con nosotros");
 
 const enlace = document.querySelector(".navbar-brand");
 enlace.remove()
 
 let agregado = document.createElement("p");
-agregado.innerHTML = "<h5>Parrafos Agregados</h5>";
+agregado.innerHTML = "<h5>Parrafo agregados</h5>";
 
 const encabezado = document.querySelector(".encabezado");
 console.log(encabezado.children)
@@ -141,3 +188,5 @@ for (let servicio of otrosServicios) {
     listado.innerHTML = servicio;
     listaVacia.appendChild(listado);
 }
+
+
